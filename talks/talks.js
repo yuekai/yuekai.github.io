b803724a95352c29c5db5a07856279590b1816ec
Yuekai Sun <yuekai@gmail.com>
2022-05-03 05:23:05 -1000

    add CV update papers

---
+++ talks/talks.js
@@ -0,0 +1,254 @@
+var talks = [
+  [
+    "Statistical perspectives on federated learning",
+    "NIST AI Metrology presentation series<br />Apr 21, 2022",
+    -77.1929215,
+    39.1399187
+  ],
+  [
+    "Learning under label shift and posterior drift",
+    "UF AI Advances & Applications seminar<br />Mar 18, 2022",
+    -82.3249846,
+    29.6519684
+  ],
+  [
+    "Does enforcing fairness mitigate algorithmic biases due to distribution shift?",
+    "UF Statistics winter workshop<br />Jan 15, 2022",
+    -82.3249846,
+    29.6519684
+  ],
+  [
+    "Does enforcing fairness mitigate algorithmic biases due to distribution shift",
+    "ICSA 2021<br />Sep 15, 2021",
+    59.9823317,
+    57.9110833
+  ],
+  [
+    "There is no trade off: enforcing fairness can improve accuracy",
+    "JSM 2021<br />Aug 8, 2021",
+    -122.3300624,
+    47.6038321
+  ],
+  [
+    "There is no trade off: enforcing fairness can improve accuracy",
+    "Algorithms for Threat Detection annual workshop<br />Nov 10, 2020",
+    -77.0365427,
+    38.8950368
+  ],
+  [
+    "Communicative efficient integrative regression in high dimensions",
+    "SIAM MDS 2020<br />May 5, 2020",
+    -84.5124602,
+    39.1014537
+  ],
+  [
+    "Training individually fair ML systems with Sensitive Subspace Robustness",
+    "IISA 2019<br />Dec 28, 2019",
+    72.8773928,
+    19.0759899
+  ],
+  [
+    "Training individually fair ML systems with Sensitive Subspace Robustness",
+    "Algorithms for Threat Detection + Algorithms for Modern Power Systems annual workshop<br />Oct 21, 2019",
+    -77.0365427,
+    38.8950368
+  ],
+  [
+    "Training individually fair ML systems with Sensitive Subspace Robustness",
+    "IBM Research - Cambridge<br />Oct 15, 2019",
+    -71.1056157,
+    42.3750997
+  ],
+  [
+    "Training individually fair ML systems with Sensitive Subspace Robustness",
+    "Cornell SCAN seminar<br />Sep 9, 2019",
+    -76.54831458600927,
+    42.437072
+  ],
+  [
+    "Statistical notions of fairness in machine learning",
+    "UC Riverside statistics department seminar<br />Apr 2, 2019",
+    -117.41024442439222,
+    33.9459413
+  ],
+  [
+    "Three vignettes in data science",
+    "Fudan statistics department short course<br />Jun 2019",
+    121.4692071,
+    31.2322758
+  ],
+  [
+    "Geometric inference in admixture models",
+    "CMStatistics 2018<br />Dec 15, 2018",
+    10.4018624,
+    43.7159395
+  ],
+  [
+    "Fast convergence of proximal Newton-type methods in high dimensions",
+    "EcoSta 2018<br />Jun 21, 2018",
+    114.1628131,
+    22.2793278
+  ],
+  [
+    "Interpreting the spectral embedding in spectral clustering",
+    "ISNPS 2018<br />Jun 2018",
+    15.310756230322482,
+    40.419441649999996
+  ],
+  [
+    "Interpreting the spectral embedding in spectral clustering",
+    "Wharton statistics department seminar<br />Nov 8, 2017",
+    -75.1635262,
+    39.9527237
+  ],
+  [
+    "Statistical notions of fairness in machine learning",
+    "2017 IMS China<br />Jun 2018",
+    108.3627211,
+    22.8193063
+  ],
+  [
+    "Fast convergence of proximal Newton-type methods in high dimensions",
+    "SIAM OP17<br />May 24, 2017",
+    -123.113952,
+    49.2608724
+  ],
+  [
+    "Interpreting the spectral embedding in spectral clustering",
+    "10th ICSA international conference<br />Dec 22, 2016",
+    121.4692071,
+    31.2322758
+  ],
+  [
+    "Feature distributed sparse regression",
+    "MOPTA 2016<br />Aug 18, 2016",
+    -75.3786521,
+    40.6178915
+  ],
+  [
+    "Fast convergence of proximal Newton-type methods in high dimensions",
+    "WHOA PSI 2016<br />Sep 20, 2016",
+    -90.1995853,
+    38.6264256
+  ],
+  [
+    "Feature distributed sparse regression",
+    "SLDS 2016<br />Jun 8, 2016",
+    -79.05578,
+    35.9131542
+  ],
+  [
+    "Exact post selection inference for sparse regression problems",
+    "Rice statistics department seminar<br />2016",
+    -95.3676974,
+    29.7589382
+  ],
+  [
+    "Communication efficient sparse regression: a one shot approach",
+    "Ford Greenfield Labs<br />2016",
+    -122.1598465,
+    37.4443293
+  ],
+  [
+    "Communication efficient sparse regression: a one shot approach",
+    "Stanford LA/OPT seminar<br />Apr 2015",
+    -122.17029258409187,
+    37.426540700000004
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "USC ISE department seminar<br />Feb 26, 2015",
+    -118.242766,
+    34.0536909
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Rutgers Department of Statistics and Biostatistics seminar<br />Feb 24, 2015",
+    -74.4660408,
+    40.5462553
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Chicago Booth statistics and econometrics seminar<br />Feb 2015",
+    -87.6244212,
+    41.8755616
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Cornell statistics department seminar<br />Feb 2015",
+    -76.54831458600927,
+    42.437072
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Carnegie Mellon statistics seminar<br />Feb 7, 2015",
+    -79.9900861,
+    40.4416941
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "IU Bloomington statistics department seminar<br />Feb 4, 2015",
+    -86.5342881,
+    39.1670396
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "McGill statistics seminar<br />Jan 30, 2015",
+    -73.5698065,
+    45.5031824
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Michigan State CMSE colloquium<br />Jan 2015",
+    -84.5553805,
+    42.7337712
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "USC Marshall DSO statistics seminar<br />Jan 2015",
+    -118.242766,
+    34.0536909
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Michigan statistics dept seminar<br />Jan 16, 2015",
+    -83.7312291,
+    42.2681569
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "UC Santa Barbara Department of Statistics and Applied Probability seminar<br />Jan 12, 2015",
+    -119.7026673,
+    34.4221319
+  ],
+  [
+    "Distributed estimation and inference for sparse regression",
+    "Georgia Tech ISyE seminar<br />Jan 8, 2015",
+    -84.3902644,
+    33.7489924
+  ],
+  [
+    "A one shot approach to distributed sparse regression",
+    "UC Davis statistics seminar<br />Jan 5, 2015",
+    -121.744583,
+    38.545379
+  ],
+  [
+    "Exact inference for censored regression problems",
+    "JSM 2014<br />Aug 4, 2014",
+    -71.0582912,
+    42.3602534
+  ],
+  [
+    "Learning mixtures of linear classifiers",
+    "ICML 2014<br />Jun 23, 2014",
+    116.3912757,
+    39.906217
+  ],
+  [
+    "Proximal Newton type methods for minimizing composite functions",
+    "ICCOPT 2013<br />Jul 2013",
+    -9.1365919,
+    38.7077507
+  ]
+];
